abc118a2209476f866bfe023b7e6c03d8fa9194b
Roberto Melo <robertomelo822@gmail.com>
2025-04-03 14:17:43 -0300

    Update token.sol
    Ultimos ajustes para compilar a EzzeBank Token

--- token.sol
+++ token.sol
@@ -1,114 +1,176 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
-pragma solidity ^0.4.24;
- 
-//Safe Math Interface
- 
+// Safe Math Interface
 contract SafeMath {
- 
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
+    function safeAdd(uint256 a, uint256 b) public pure returns (uint256 c) {
         c = a + b;
         require(c >= a);
     }
- 
-    function safeSub(uint a, uint b) public pure returns (uint c) {
+
+    function safeSub(uint256 a, uint256 b) public pure returns (uint256 c) {
         require(b <= a);
         c = a - b;
     }
- 
-    function safeMul(uint a, uint b) public pure returns (uint c) {
+
+    function safeMul(uint256 a, uint256 b) public pure returns (uint256 c) {
         c = a * b;
         require(a == 0 || c / a == b);
     }
- 
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
+
+    function safeDiv(uint256 a, uint256 b) public pure returns (uint256 c) {
         require(b > 0);
         c = a / b;
     }
 }
- 
- 
-//ERC Token Standard #20 Interface
- 
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
- 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+// ERC Token Standard #20 Interface
+abstract contract ERC20Interface {
+    function totalSupply() public view virtual returns (uint256);
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        virtual
+        returns (uint256 balance);
+
+    function allowance(address tokenOwner, address spender)
+        public
+        view
+        virtual
+        returns (uint256 remaining);
+
+    function transfer(address to, uint256 tokens)
+        public
+        virtual
+        returns (bool success);
+
+    function approve(address spender, uint256 tokens)
+        public
+        virtual
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public virtual returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
 }
- 
- 
-//Contract function to receive approval and execute function in one call
- 
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+
+// Contract function to receive approval and execute function in one call
+abstract contract ApproveAndCallFallBack {
+    function receiveApproval(
+        address from,
+        uint256 tokens,
+        address token,
+        bytes calldata data
+    ) public virtual; // Adicionado 'virtual'
 }
- 
-//Actual token contract
- 
+
+// Actual token contract
 contract EZZEBANKToken is ERC20Interface, SafeMath {
     string public symbol;
-    string public  name;
+    string public name;
     uint8 public decimals;
-    uint public _totalSupply;
- 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
- 
-    constructor() public {
+    uint256 public _totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    constructor() {
         symbol = "EZZEBANK";
         name = "EZZEBANK Coin";
         decimals = 2;
         _totalSupply = 100000;
         balances[0x1e36027e619660CCccCd8db851EEcf27dAaf5286] = _totalSupply;
-        emit Transfer(address(0), 0x1e36027e619660CCccCd8db851EEcf27dAaf5286, _totalSupply);
+        emit Transfer(
+            address(0),
+            0x1e36027e619660CCccCd8db851EEcf27dAaf5286,
+            _totalSupply
+        );
     }
- 
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply  - balances[address(0)];
+
+    function totalSupply() public override view returns (uint256) {
+        return _totalSupply - balances[address(0)];
     }
- 
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+
+    function balanceOf(address tokenOwner) override
+        public
+        view
+        returns (uint256 balance)
+    {
         return balances[tokenOwner];
     }
- 
-    function transfer(address to, uint tokens) public returns (bool success) {
+
+    function transfer(address to, uint256 tokens)override
+        public
+        returns (bool success)
+    {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
- 
-    function approve(address spender, uint tokens) public returns (bool success) {
+
+    function approve(address spender, uint256 tokens)override
+        public
+        returns (bool success)
+    {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
- 
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens 
+    ) override public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(from, to, tokens);
         return true;
     }
- 
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+
+    function allowance(address tokenOwner, address spender) override
+        public
+        view
+        returns (uint256 remaining)
+    {
         return allowed[tokenOwner][spender];
     }
- 
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+
+    function approveAndCall(
+        address spender,
+        uint256 tokens,
+        bytes calldata data
+    ) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        ApproveAndCallFallBack(spender).receiveApproval(
+            msg.sender,
+            tokens,
+            address(this),
+            data
+        );
         return true;
     }
- 
-    function () public payable {
-        revert();
+
+    // Função fallback para rejeitar transações em Ether
+    fallback() external payable {
+        revert("This contract does not accept plain Ether.");
     }
-} 
+
+    // Função receive para rejeitar transações de Ether diretas
+    receive() external payable {
+        revert("This contract does not accept plain Ether.");
+    }
+}
